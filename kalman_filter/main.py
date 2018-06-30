eb150061d41026f81af10b62aae26f644010fba5
oleg-desktop <archonoff@gmail.com>
2018-06-30 04:38:37 +0300

    common code for kalman filter running

--- kalman_filter/main.py
+++ kalman_filter/main.py
@@ -2,11 +2,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 from kalman_filter.simple_copter import SimpleCopter
-from kalman_filter.kalman_filters import only_omega_filter
+from kalman_filter.kalman_filters import only_omega_filter, angular_1d_filter
 
 
-def demo_run():
+def filter_1d_run():
     true_alphas = []
+    true_alphas2 = []
     measured_omegas = []
     measured_axs = []
     measured_ays = []
@@ -14,6 +15,52 @@ def demo_run():
     copter = SimpleCopter()
     for _ in range(1000):
         true_alphas.append(copter.alpha_x)
+        true_alphas2.append(copter.alpha_from_a())
+        omega, a_x, a_y = copter.sense()
+        measured_omegas.append(omega)
+        measured_axs.append(a_x)
+        measured_ays.append(a_y)
+        copter.step()
+
+    filtered_alphas, filtered_axs, filtered_ays = angular_1d_filter(measurements=zip(measured_omegas, measured_axs, measured_ays))
+
+    figure, axes = plt.subplots(nrows=2, ncols=1, sharex=True, gridspec_kw={'height_ratios': (5, 1)})
+    figure.set_figheight(8)
+    figure.set_figwidth(10)
+
+    ax1, ax2 = axes
+
+    ax1.plot(true_alphas, label='Истиный угол')
+    ax1.plot(true_alphas2, label='Истиный угол2')
+    ax1.plot(measured_omegas, label='Измеренная скорость')
+    ax1.plot(filtered_alphas, label='Вычисленный угол', c='r')
+    ax1.set_ylabel('Угол/угловая скорость')
+
+    ax2.plot(measured_axs, label='Измеренная проекция ax')
+    ax2.plot(measured_ays, label='Измеренная проекция ay')
+    ax2.plot(filtered_axs, label='Отфильтрованная проекция ax')
+    ax2.plot(filtered_ays, label='Отфильтрованная проекция ay')
+    ax2.set_xlabel('Время')
+    ax2.set_ylabel('Вектор ускорения')
+
+    for axis in axes:
+        axis.grid()
+        axis.legend()
+
+    plt.show()
+
+
+def demo_run():
+    true_alphas = []
+    true_alphas2 = []
+    measured_omegas = []
+    measured_axs = []
+    measured_ays = []
+
+    copter = SimpleCopter()
+    for _ in range(1000):
+        true_alphas.append(copter.alpha_x)
+        true_alphas2.append(copter.alpha_from_a())
         omega, a_x, a_y = copter.sense()
         measured_omegas.append(omega)
         measured_axs.append(a_x)
@@ -27,8 +74,9 @@ def demo_run():
     ax1, ax2 = axes
 
     ax1.plot(true_alphas, label='Истиный угол')
+    ax1.plot(true_alphas2, label='Истиный угол (через арктангенс)')
     ax1.plot(measured_omegas, label='Измеренная скорость')
-    ax2.set_ylabel('Угол/угловая скорость')
+    ax1.set_ylabel('Угол/угловая скорость')
 
     ax2.plot(measured_axs, label='Измеренная проекция ax')
     ax2.plot(measured_ays, label='Измеренная проекция ay')
@@ -69,7 +117,8 @@ def only_omega_run():
 
 
 def main():
-    demo_run()
+    # demo_run()
+    filter_1d_run()
 
 
 if __name__ == '__main__':
